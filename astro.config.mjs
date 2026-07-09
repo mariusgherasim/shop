78ccdc5586226d3f802d008db21016dc5dfa208d
mariusgherasim <133738250+mariusgherasim@users.noreply.github.com>
2026-07-09 21:08:15 +0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://shop.gherasimmarius.com',
   output: 'static',
   trailingSlash: 'ignore',
+  integrations: [sitemap()],
 });
